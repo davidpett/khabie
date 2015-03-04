945f1bf8ddc8bdf2735193048852761a331da570
David Pett <me@davidpett.com>
2015-03-04 16:02:52 -0600

    recently heard

--- app/controllers/sayings.js
+++ app/controllers/sayings.js
@@ -1,26 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  sayingText: null,
-
-  actions: {
-    createSaying: function() {
-      var saying = this.get('model').findBy('text', this.get('sayingText')),
-          time = moment.utc().format();
-
-      if (!saying) {
-        saying = this.store.createRecord('saying', {
-          text: this.get('sayingText'),
-          times: Ember.A(),
-          lastTime: time
-        });
-      }
-
-      saying.get('times').pushObject(time);
-      saying.save();
-
-
-      this.set('sayingText', null);
-    }
-  }
 });
--- app/controllers/sayings/index.js
+++ app/controllers/sayings/index.js
@@ -1,10 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  sortProp: ['count:desc'],
-  sortedItems: Ember.computed.sort('model', 'sortProp'),
+  sayingText: null,
+
+  items: Ember.computed.readOnly('model'),
+
+  popularSortProp: ['count:desc'],
+  popularSortedItems: Ember.computed.sort('items', 'popularSortProp'),
+  popularTopItems: function() {
+    return this.get('popularSortedItems').slice(0, 3);
+  }.property('popularSortedItems'),
+
+
+  recentSortProp: ['lastTime:desc'],
+  recentSortedItems: Ember.computed.sort('items', 'recentSortProp'),
+  recentTopItems: function() {
+    return this.get('recentSortedItems').slice(0, 5);
+  }.property('recentSortedItems.@each.lastTime'),
+
 
   actions: {
+    createSaying: function() {
+      var saying = this.get('model').findBy('text', this.get('sayingText')),
+          time = moment.utc().format();
+
+      if (!saying) {
+        saying = this.store.createRecord('saying', {
+          text: this.get('sayingText'),
+          times: Ember.A()
+        });
+      }
+      saying.set('lastTime', time);
+      saying.get('times').pushObject(time);
+      saying.save();
+
+
+      this.set('sayingText', null);
+    },
+
     gotoItem: function(value) {
       this.transitionToRoute('saying', value);
     }
